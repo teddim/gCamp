ea660e4462504763981854bbe972acabff370012
Teddi Maull <teddi@picturetheday.com>
2015-01-29 17:28:05 -0700

    Add FAQ page to the app

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root "welcome#index"
    get '/terms' => 'terms#index'
-   get '/about' => 'about#index'   
+   get '/about' => 'about#index'
+   get '/faq' => 'faq#index'   
 
 
   # Example of regular route:
