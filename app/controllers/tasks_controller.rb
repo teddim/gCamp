a698adb3d3af221954e533e06756fae82b422487
Teddi Maull <teddi@picturetheday.com>
2015-02-05 22:55:26 -0700

    get functionality of new task page working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,20 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
 end
