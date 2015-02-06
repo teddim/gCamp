9a3cfc811999699b4535cb494db37fb9d02eebce
Teddi Maull <teddi@picturetheday.com>
2015-02-05 23:05:34 -0700

    get Edit page functionality working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,20 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      redirect_to task_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def task_params
