1b25e7b434a0486d304c55f8f64e1ded30e13844
Teddi Maull <teddi@picturetheday.com>
2015-02-05 22:04:28 -0700

    Get index page working, add a few items to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+tasks = Task.create([{ description: 'this is my first task' }, { description: 'this is another task' }])
