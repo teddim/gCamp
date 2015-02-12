b648ee37dcec4de07b1edbed322196c0e33d3aae
Teddi Maull <teddi@picturetheday.com>
2015-02-12 14:30:40 -0700

    Add Due Date to the Show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,7 +47,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:description, :complete)
+    params.require(:task).permit(:description, :complete, :due_date)
   end
 
 end
