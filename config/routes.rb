46e42fc4bb630be7a20fa885245edec1e711cc7b
Teddi Maull <teddi@picturetheday.com>
2015-02-12 16:16:53 -0700

    Add Users link in the footer

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :tasks
+  resources :users, only: [:index]
 
   # Example resource route with options:
   #   resources :products do
