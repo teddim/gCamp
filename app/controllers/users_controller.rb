41d20795acabbc704ab1b51c48e2265a0fb71364
Teddi Maull <teddi@picturetheday.com>
2015-02-17 16:24:13 -0700

    Add delete button to edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
 
   private
 
