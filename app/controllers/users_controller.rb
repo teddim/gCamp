6d61d912ad3aeea51b37735363ad9b4ca1557c36
Teddi Maull <teddi@picturetheday.com>
2015-02-17 15:41:33 -0700

    Users can update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,20 @@ class UsersController < ApplicationController
   #     @user = User.find(params[:id])
   # end
 
+  def edit
+    @user = User.find(params[:id])
+
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      flash[:notice] = "User was successfully updated"
+    redirect_to users_path
+    end
+  end
+
+
   private
 
   def user_params
