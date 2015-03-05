e26224ac8da81d3e72b48df80a1aeea48dc1080e
Teddi Maull <teddi@picturetheday.com>
2015-03-05 15:30:59 -0700

    Add new method

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,8 @@
 class AuthenticationController < ApplicationController
 
+  def new
+    @user = User.new
+  end
 
   def create
     user = User.find_by_email(params[:email])
@@ -7,7 +10,7 @@ class AuthenticationController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path
     else
-      @sign_in_error = "Username/password combination is invalid"
+      flash[:error] = "Username/password combination is invalid"
       render :new
     end
   end
