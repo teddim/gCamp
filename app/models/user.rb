4179bced3d9bfc51b43240290a2e9c3a88680268
Teddi Maull <teddi@picturetheday.com>
2015-03-06 13:25:57 -0700

    Fix validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  validates :first_name, :last_name, :email, presence: true
+
+  validates :first_name, :last_name,:email, presence: true
   validates :email, uniqueness: true
+
+  has_secure_password
 end
