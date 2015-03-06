2c4f339b1f70ee6262fd750212538a0a3fdd4c40
Teddi Maull <teddi@picturetheday.com>
2015-03-06 13:21:47 -0700

    Bootstrap the signin page

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -8,15 +8,17 @@ class AuthenticationController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      flash[:notice] = "You have successfully signed up"
       redirect_to root_path
     else
-      flash[:error] = "Username/password combination is invalid"
+      flash[:error] = "Email / Password combination is invalid"
       render :new
     end
   end
 
   def destroy
     session.clear
+    flash[:notice] = "You have successfully logged out"
     redirect_to root_path
   end
 
