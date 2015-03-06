2ff3df5dd064d293d713acd411e65990beec686f
Teddi Maull <teddi@picturetheday.com>
2015-03-06 13:20:44 -0700

    Fix whitespace

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,17 +8,14 @@ class ApplicationController < ActionController::Base
 
   def current_user
     User.find_by(id: session[:user_id])
-
   end
 
   def authenticate_user
     if current_user
-
     else
       flash[:error] = "You must sign in"
       redirect_to signin_path
     end
   end
-
-
+  
 end
