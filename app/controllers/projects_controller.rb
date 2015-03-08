995678db231a6afa95c804352740552db2187095
Teddi Maull <teddi@picturetheday.com>
2015-03-08 11:45:10 -0600

    Fix redirect from update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -29,7 +29,7 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = "Project was successfully updated"
-      redirect_to project_path(project)
+      redirect_to project_path(@project)
     else
       render :edit
     end
