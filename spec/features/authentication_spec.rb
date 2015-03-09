8879fc3e14dd3a0b3d7a370315a5219cdc51e20b
Teddi Maull <teddi@picturetheday.com>
2015-03-09 13:57:02 -0600

    Add Authorization spec

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -12,3 +12,28 @@ feature 'Sign Out button-' do
   end
 
 end
+
+feature 'Sign In -' do
+
+  scenario 'Guest can sign in successfully' do
+    create_user
+    visit root_path
+    within('ul.navbar-right') do
+      click_on 'Sign In'
+    end
+    fill_in 'Email', with: 't@t.com'
+    fill_in 'Password', with: 'test'
+    click_on 'Sign in'
+
+    expect(page).to have_content('Tester Test')
+    expect(page).to have_content('You have successfully signed in')
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Guest can\'t sign in without entering required data' do
+    create_user
+    visit signin_path
+    click_on 'Sign in'
+    expect(page).to have_content("Email / Password combination is invalid")
+  end
+end
