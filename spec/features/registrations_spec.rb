b842e0558a2e41eca21724f01643abb747d418be
Teddi Maull <teddi@picturetheday.com>
2015-03-09 13:58:15 -0600

    Fix validation spec

--- spec/features/registrations_spec.rb
+++ spec/features/registrations_spec.rb
@@ -20,15 +20,19 @@ feature 'Sign Up page-' do
   end
 
   scenario 'User can\'t create a new account without entering required data' do
-    login
+    create_user
     visit signup_path
-    click_on 'Sign Up'
+    within(".form-horizontal") do
+      click_on 'Sign Up'
+    end
     expect(page).to have_content("First name can't be blank")
     expect(page).to have_content("Last name can't be blank")
     expect(page).to have_content("Email can't be blank")
     expect(page).to have_content("Password can't be blank")
     fill_in 'Email', with: 't@t.com'
-    click_on 'Sign Up'
+    within(".form-horizontal") do
+      click_on 'Sign Up'
+    end
     expect(page).to have_content("Email has already been taken")
 
 
