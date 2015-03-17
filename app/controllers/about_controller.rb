dca2abe64c2a449bf4be34b61972b3b473dad4bb
Teddi Maull <teddi@picturetheday.com>
2015-03-16 21:14:37 -0600

    add stats to the page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,9 @@
 class AboutController < ApplicationController
   def index
-
+    @project_count = Project.all.count
+    @task_count = Task.all.count
+    @membership_count = Membership.all.count
+    @user_count = User.all.count
+    @comment_count = Comment.all.count
   end
 end
