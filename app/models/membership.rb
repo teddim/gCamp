a418b7e140cbfcddc80e8e933d6d83da571b6d2a
Teddi Maull <teddi@picturetheday.com>
2015-03-20 18:57:49 -0600

    Allow users to be added to more than one project

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,7 @@ class Membership < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :project
-  validates :user, presence: true, uniqueness: {message: "has already been added to this project"}
+  validates :user, presence: true, uniqueness: {scope: :project_id, message: "has already been added to this project"}
 
   OWNER = 'owner'
   MEMBER = 'member'
