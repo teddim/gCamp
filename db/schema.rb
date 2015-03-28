1a59eb0948798c21779d08b9e05e074e1a32f1a1
Teddi Maull <teddi@picturetheday.com>
2015-03-27 21:29:54 -0600

    add admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315065308) do
+ActiveRecord::Schema.define(version: 20150328024854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,10 +44,11 @@ ActiveRecord::Schema.define(version: 20150315065308) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.text   "first_name"
-    t.text   "last_name"
-    t.text   "email"
-    t.string "password_digest"
+    t.text    "first_name"
+    t.text    "last_name"
+    t.text    "email"
+    t.string  "password_digest"
+    t.boolean "admin",           default: false, null: false
   end
 
 end
